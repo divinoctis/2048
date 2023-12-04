7d1eaeaa84972c58c56d027bd752778dea7be5c4
Zeno <mdubos@gaming.tech>
2023-12-04 14:29:13 +0100

    FF

--- 2048.py
+++ 2048.py
@@ -2,34 +2,42 @@ import random
 import os
 
 def board():
-    issue = [
-        [0,None], [1,None], [2,None], [3,None],
-        [10, None], [11, None], [12, None], [13, None],
-        [20,None], [21,None], [22, None], [23, None],
-        [30, None], [31, None], [32, None], [33, None],
-    ]
+    issue: list[list[int]] = []
+    for i in range(0, 3):
+        issue.append([] * 4)
 
-def randomShots():
-    random: int = [0,1,2,3,4,5,6,7,8,9]
-    value: int = random.randint([])
-    if value > 0:
-        value = 2
+def randomShots(issue):
+    rate: list[int] = [0,1,2,3,4,5,6,7,8,9]
+    valueRandom: int = random.randint(rate[0, 9])
+    if valueRandom > 0:
+        value: int = 2
     else:
-        value = 4
+        value: int = 4
+    i: int = random.randint(rate[0, 3])
+    j: int = random.randint(rate[0, 3])
+    issue[i][j].append(value)
+
+'''
+def fusion():
+'''
 
 def move(issue):    
     n = 0
     button : str = None
     while button != "Q" or button !="D" or button !="S":
-        button: str = input("left: Q / right: D / down: S \n").upper()
+        button: str = input("left: Q / right: D / down: S / up: Z \n").upper()
     for i in range(3):
         if button == "D":   
-                issue[n+1][1] = issue[n][1]+ issue[n+1][1]
+                issue[n][1] = issue[n][1] + issue[n+1][1]
         elif button == "Q":
-            issue[n-1][1] = issue[n][1]+ issue[n-1][1]
+            issue[n+1][1] = issue[n][1] + issue[n+1][1]
         elif button == "S":
-            issue[n+10][1] = issue[n][1]+ issue[n+10][1]
+            issue[n+10][1] = issue[n][1] + issue[n+10][1]
+        elif button == "Z":
+            issue[n][1] = issue[n][1] + issue[n+10][1]
 move()
 
 def game():
     os.system("cls")
+
+game()
