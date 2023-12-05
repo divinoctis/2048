995177b551c94c3496ea3e62166b4dcc17014bee
Zeno <mdubos@gaming.tech>
2023-12-05 09:24:55 +0100

    aa

--- 2048.py
+++ 2048.py
@@ -2,42 +2,50 @@ import random
 import os
 
 def board():
-    issue: list[list[int]] = []
-    for i in range(0, 3):
-        issue.append([] * 4)
+    issue = []
+
+    for i in range(0, 4):
+        issue.append([0] * 4)
+    return issue
 
 def randomShots(issue):
-    rate: list[int] = [0,1,2,3,4,5,6,7,8,9]
-    valueRandom: int = random.randint(rate[0, 9])
-    if valueRandom > 0:
-        value: int = 2
-    else:
-        value: int = 4
-    i: int = random.randint(rate[0, 3])
-    j: int = random.randint(rate[0, 3])
-    issue[i][j].append(value)
+    for i in range(2):
+        valueRandom: int = random.randint(0,9)
+        if valueRandom > 0:
+            value: int = 2
+        else:
+            value: int = 4
+        i : int = random.randint(0,3)
+        j : int = random.randint(0,3)
+        issue[i][j] = value
+
+def displayBoard(issue):
+    for row in issue:
+       print(row)
+
+def move(issue):
+    n: int = 0
+    button: str = None
+    while button not in ["Q", "D", "S", "Z"]:
+        button = input("left: Q / right: D / down: S / up: Z \n").upper()
+    for i in range(3):
+        if button == "D":
+            issue[n][1] = issue[n][1] + issue[n + 1][1]
+        elif button == "Q":
+            issue[n + 1][1] = issue[n][1] + issue[n + 1][1]
+        elif button == "S":
+            issue[n + 10][1] = issue[n][1] + issue[n + 10][1]
+        elif button == "Z":
+            issue[n][1] = issue[n][1] + issue[n + 10][1]
 
 '''
 def fusion():
 '''
 
-def move(issue):    
-    n = 0
-    button : str = None
-    while button != "Q" or button !="D" or button !="S":
-        button: str = input("left: Q / right: D / down: S / up: Z \n").upper()
-    for i in range(3):
-        if button == "D":   
-                issue[n][1] = issue[n][1] + issue[n+1][1]
-        elif button == "Q":
-            issue[n+1][1] = issue[n][1] + issue[n+1][1]
-        elif button == "S":
-            issue[n+10][1] = issue[n][1] + issue[n+10][1]
-        elif button == "Z":
-            issue[n][1] = issue[n][1] + issue[n+10][1]
-move()
-
 def game():
     os.system("cls")
+    myBoard = board()
+    randomShots(myBoard)
+    displayBoard(myBoard)
 
 game()
